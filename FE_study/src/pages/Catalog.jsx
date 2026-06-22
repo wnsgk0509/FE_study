c612d6f9edded928074adec01e227bf775b2b486
coehdgk0330 <coehdgk0330@gmail.com>
2026-06-22 14:43:55 +0900

    카탈로그 상세페이지 작업

--- FE_study/src/pages/Catalog.jsx
+++ FE_study/src/pages/Catalog.jsx
@@ -9,11 +9,10 @@ function Catalog(){
         <nav style={{ padding: '10px', backgroundColor: '#eee', marginBottom: '20px' }}>
             <Link to="/catalog/catalogDetail"><button>카탈로그 상세페이지</button></Link>
             </nav>
-
             
 
-
         </div>
+        
     )
 }
 
--- FE_study/src/pages/CatalogDetail.jsx
+++ FE_study/src/pages/CatalogDetail.jsx
@@ -1,7 +1,39 @@
+import "./CatalogDetail.css"
+
 function CatalogDetail(){
     return(
         <>
-            <h2>카탈로그 상세페이지</h2>
+            {/* 메인 배너 */}
+            <div className="mainBanner">
+                <p className="main11">학습 카탈로그</p>
+                <p className="main12">총 12개의 코스가 준비되어 있습니다.</p>
+            </div>
+            {/* 학습 과목 선택 */}
+            <div className="main2Banner">
+                <button className="btn1">전체</button>
+                <button className="btn2">HTML</button>
+                <button className="btn3">CSS</button>
+                <button className="btn4">JS</button>
+                <button className="btn5">React</button>
+            </div>
+            <div>
+                <div className="studyBox "></div>
+                <div className="studyBox"></div>
+                <div className="studyBox"></div>
+                <div className="studyBox"></div>
+                <div className="studyBox"></div>
+                <div className="studyBox"></div>
+                <div className="studyBox"></div>
+                <div className="studyBox"></div>
+                <div className="studyBox"></div>
+                <div className="studyBox"></div>
+                <div className="studyBox"></div>
+                <div className="studyBox"></div>
+
+
+            </div>
+
+
         </>
     )
 }
