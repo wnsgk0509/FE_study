cbac4c73da533887ca3627acd681ed2615290364
coehdgk0330 <coehdgk0330@gmail.com>
2026-06-22 14:55:28 +0900

    카탈로그 페이지 작업

--- FE_study/src/pages/Catalog.jsx
+++ FE_study/src/pages/Catalog.jsx
@@ -1,4 +1,5 @@
 import {Link } from 'react-router-dom';
+import "./Catalog.css"
 
 
 
@@ -6,10 +7,36 @@ import {Link } from 'react-router-dom';
 function Catalog(){
     return(
         <div>
-        <nav style={{ padding: '10px', backgroundColor: '#eee', marginBottom: '20px' }}>
             <Link to="/catalog/catalogDetail"><button>카탈로그 상세페이지</button></Link>
-            </nav>
-            
+             {/* 메인 배너 */}
+            <div className="mainBanner">
+                <p className="main11">학습 카탈로그</p>
+                <p className="main12">총 12개의 코스가 준비되어 있습니다.</p>
+            </div>
+            {/* 학습 과목 선택 */}
+            <div className="main2Banner">
+                <button className="btn1">전체</button>
+                <button className="btn2">HTML</button>
+                <button className="btn3">CSS</button>
+                <button className="btn4">JS</button>
+                <button className="btn5">React</button>
+            </div>
+            <div>
+                <div className="studyBox "></div>
+                <div className="studyBox"></div>
+                <div className="studyBox"></div>
+                <div className="studyBox"></div>
+                <div className="studyBox"></div>
+                <div className="studyBox"></div>
+                <div className="studyBox"></div>
+                <div className="studyBox"></div>
+                <div className="studyBox"></div>
+                <div className="studyBox"></div>
+                <div className="studyBox"></div>
+                <div className="studyBox"></div>
+
+
+            </div>
 
         </div>
         
--- FE_study/src/pages/CatalogDetail.jsx
+++ FE_study/src/pages/CatalogDetail.jsx
@@ -1,37 +1,8 @@
-import "./CatalogDetail.css"
 
 function CatalogDetail(){
     return(
         <>
-            {/* 메인 배너 */}
-            <div className="mainBanner">
-                <p className="main11">학습 카탈로그</p>
-                <p className="main12">총 12개의 코스가 준비되어 있습니다.</p>
-            </div>
-            {/* 학습 과목 선택 */}
-            <div className="main2Banner">
-                <button className="btn1">전체</button>
-                <button className="btn2">HTML</button>
-                <button className="btn3">CSS</button>
-                <button className="btn4">JS</button>
-                <button className="btn5">React</button>
-            </div>
-            <div>
-                <div className="studyBox "></div>
-                <div className="studyBox"></div>
-                <div className="studyBox"></div>
-                <div className="studyBox"></div>
-                <div className="studyBox"></div>
-                <div className="studyBox"></div>
-                <div className="studyBox"></div>
-                <div className="studyBox"></div>
-                <div className="studyBox"></div>
-                <div className="studyBox"></div>
-                <div className="studyBox"></div>
-                <div className="studyBox"></div>
-
-
-            </div>
+        
 
 
         </>
