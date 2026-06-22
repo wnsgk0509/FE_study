8810ebcae198d0876b16466a440975938af10fa7
seojunho577 <sjh051126@gmail.com>
2026-06-22 16:02:55 +0900

    커뮤니티 페이지 디자인 및 페이지 이동

--- FE_study/src/pages/Catalog.jsx
+++ FE_study/src/pages/Catalog.jsx
@@ -1,16 +1,16 @@
-import {Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 
 
-function Catalog(){
-    return(
+function Catalog() {
+    return (
         <div>
-        <nav style={{ padding: '10px', backgroundColor: '#eee', marginBottom: '20px' }}>
-            <Link to="/catalog/catalogDetail"><button>카탈로그 상세페이지</button></Link>
+            <nav style={{ padding: '10px', backgroundColor: '#eee', marginBottom: '20px' }}>
+                <Link to="/catalog/catalogDetail"><button>카탈로그 상세페이지</button></Link>
             </nav>
 
-            
+
 
 
         </div>
