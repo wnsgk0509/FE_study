9e8a1722f9d226c92dbff7c1331dfff35946ed8f
coehdgk0330 <coehdgk0330@gmail.com>
2026-06-22 16:22:36 +0900

    카탈로그 페이지 작업

---
+++ FE_study/src/pages/catalogPage/StudyBox.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import '../catalogPage/StudyBox.css';
+import { Link } from "react-router-dom";
+
+function StudyBox ({text1 , text2 , difficult , explain , time}){
+    return(
+        <>
+        <Link to="/catalog/catalogDetail"><button className="studyBox">
+            <p className="study1">{text1}</p>
+            <p className="study2">{text2}</p>    
+            <p className="study3">{difficult}</p>    
+            <p className="study4">{explain}</p>    
+            <p className="study5">{time}</p>    
+
+        </button></Link>
+        </>
+    )
+}
+export default StudyBox;
