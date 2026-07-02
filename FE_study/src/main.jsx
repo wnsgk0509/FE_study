748c9041eb929d7e1beeccf280502ad234294a24
wnsgk0509 <rndrndqkd@gmail.com>
2026-07-02 11:59:36 +0900

    깃허브 서버에 배포

--- FE_study/src/main.jsx
+++ FE_study/src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </StrictMode>,
